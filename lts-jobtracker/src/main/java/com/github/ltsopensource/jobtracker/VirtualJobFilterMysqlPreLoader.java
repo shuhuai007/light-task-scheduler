2dab68f914b49c91a003b6113a6c4f66e8b41b38
Jie Zhou <zhoujie338@126.com>
2016-05-26 15:03:33 +0800

    add log info

--- lts-jobtracker/src/main/java/com/github/ltsopensource/jobtracker/VirtualJobFilterMysqlPreLoader.java
+++ lts-jobtracker/src/main/java/com/github/ltsopensource/jobtracker/VirtualJobFilterMysqlPreLoader.java
@@ -1,5 +1,7 @@
 package com.github.ltsopensource.jobtracker;
 
+import com.github.ltsopensource.core.logger.Logger;
+import com.github.ltsopensource.core.logger.LoggerFactory;
 import com.github.ltsopensource.core.support.JobUtils;
 import com.github.ltsopensource.jobtracker.domain.JobTrackerAppContext;
 import com.github.ltsopensource.jobtracker.support.VirtualJobResolver;
@@ -13,6 +15,9 @@ import java.util.List;
  * PreLoader to handle virtual node when loading.
  */
 public class VirtualJobFilterMysqlPreLoader extends MysqlPreLoader {
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(VirtualJobFilterMysqlPreLoader.class);
+
     private JobTrackerAppContext jobTrackerAppContext;
     public VirtualJobFilterMysqlPreLoader(JobTrackerAppContext appContext) {
         super(appContext);
@@ -21,8 +26,10 @@ public class VirtualJobFilterMysqlPreLoader extends MysqlPreLoader {
 
     @Override
     protected List<JobPo> load(String loadTaskTrackerNodeGroup, int loadSize) {
+        LOGGER.info("enter " + Thread.currentThread().getStackTrace()[1].getMethodName());
         List<JobPo> actualNodeList = new ArrayList<JobPo>();
         List<JobPo> jobPoList = super.load(loadTaskTrackerNodeGroup, loadSize);
+        LOGGER.info("jobPoList size:" + jobPoList.size());
         for (JobPo jobPo : jobPoList) {
             if (JobUtils.isVirtualNode(jobPo)) {
                 jobTrackerAppContext.getExecutableJobQueue().remove(jobPo.getTaskTrackerNodeGroup(),
